9c0b85856bcfa3c4766303db5f68772ebdf9b012
Rolando Sorrentino <rolando4693@gmail.com>
2023-10-18 15:39:30 -0400

    chore: :rocket: Repository created

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  useTabs: false,
+};
