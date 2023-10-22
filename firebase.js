a3bd8babf3f189753e0677e36852ab39b1bc5e7a
Rolando Sorrentino <rolando.sorrentino@correo.unimet.edu.ve>
2023-10-22 14:22:06 -0400

    fix: :bug: Fixed environmental variables, Fixed DOM invalid properties Navbar

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithPopup,
@@ -7,87 +7,80 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from 'firebase/auth';
+} from "firebase/auth";
 
-import {
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc
-} from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 import { getStorage } from "firebase/storage";
 
-
-
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
 };
 
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-  provider.setCustomParameters({
-    prompt: 'select_account',
-  });
+provider.setCustomParameters({
+  prompt: "select_account",
+});
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-  export const db = getFirestore(firebaseApp);
-  export const storage = getStorage(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
 
-  export const createUserDocumentFromAuth = async (
-    userAuth,
-    additionalInformation = {}
-  ) => {
-    if (!userAuth) return;
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
 
-    const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    const userSnapshot = await getDoc(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
 
-    if (!userSnapshot.exists()) {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
-      const typeUser = 'user'
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    const typeUser = "user";
 
-      try {
-        await setDoc(userDocRef, {
-          displayName,
-          email,
-          createdAt,
-          typeUser,
-          ...additionalInformation,
-        });
-      } catch (error) {
-        console.log('Error creando el usuario', error.message);
-      }
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        typeUser,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("Error creando el usuario", error.message);
     }
+  }
 
-    return userDocRef;
-  };
+  return userDocRef;
+};
 
-  export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-    return await createUserWithEmailAndPassword(auth, email, password);
-  };
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
-  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-    return await signInWithEmailAndPassword(auth, email, password);
-  };
+  return await signInWithEmailAndPassword(auth, email, password);
+};
 
-  export const signOutUser =  async () => await signOut(auth);
+export const signOutUser = async () => await signOut(auth);
 
-  export const onAuthStateChangedListener = (callback) =>
+export const onAuthStateChangedListener = (callback) =>
   onAuthStateChanged(auth, callback);
--- src/app/guides/page.jsx
+++ src/app/guides/page.jsx
@@ -1,48 +1,45 @@
 "use client";
 
 import GuideCard from "@/components/guideCard/guideCard";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { db } from "../../../firebase";
 import { getDocs, collection } from "firebase/firestore";
 
-
 const Manuals = () => {
-    const [manuals, setManuals] = useState([]);
+  const [manuals, setManuals] = useState([]);
 
-    useEffect(() => {
-      const fetchCategories = async () => {
-        try {
-          const manualsCollection = collection(db, "manuals");
-          const manualsSnapshot = await getDocs(manualsCollection);
-          const manualsData = manualsSnapshot.docs.map((doc) => doc.data());
-          setManuals(manualsData);
-        } catch (error) {
-          console.log("Error fetching categories: ", error);
-        }
-      };
-      fetchCategories();
-    }, []);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const manualsCollection = collection(db, "manuals");
+        const manualsSnapshot = await getDocs(manualsCollection);
+        const manualsData = manualsSnapshot.docs.map((doc) => doc.data());
+        setManuals(manualsData);
+      } catch (error) {
+        console.log("Error fetching categories: ", error);
+      }
+    };
+    fetchCategories();
+  }, []);
 
-    console.log(manuals);
+  return (
+    <>
+      <div className="h-screen pl-10 pt-28 text-2xl font-bold	">
+        <h1 className="font-['B612'] pt-5 pb-10 text-3xl">
+          Manuales de Laboratorio
+        </h1>
 
-    return(
-        <>
-        <div className="h-screen pl-10 pt-28 text-2xl font-bold	">
-            <h1 className="font-['B612'] pt-5 pb-10 text-3xl"> 
-              Manuales de Laboratorio 
-            </h1>
-
-            {manuals.map((manual, index) => (
-              <GuideCard 
-                  key={index} 
-                  title={manual.title || "Átomos y Elementos"} 
-                  subtitle={manual.subtitle || "Subhead"} 
-                  pdfUrl={manual.url} 
-              />
-           ))}
-        </div>
-        </>
-    )
-}
+        {manuals.map((manual, index) => (
+          <GuideCard
+            key={index}
+            title={manual.title || "Átomos y Elementos"}
+            subtitle={manual.subtitle || "Subhead"}
+            pdfUrl={manual.url}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Manuals;
