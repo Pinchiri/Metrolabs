b33c280daed4333c86c1c737b4783c08fae1d4e4
Rolando Sorrentino <rolando.sorrentino@correo.unimet.edu.ve>
2023-10-23 21:08:56 -0400

    feat: :sparkles: Added schedules to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,22 @@
 
 import React, { useState } from "react";
 import NavbarView from "./NavbarView";
+import { guidesURL, labURL, schedulesURL } from "@/constants/urls";
 
 const Navbar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const navbarOptions = [
     {
       name: "Manuales",
-      link: "/guides",
+      link: guidesURL,
     },
     {
       name: "Laboratorio",
-      link: "/lab",
+      link: labURL,
+    },
+    {
+      name: "Horarios",
+      link: schedulesURL,
     },
   ];
 
