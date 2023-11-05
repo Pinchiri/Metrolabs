312f84a20ed9ae214d03242620cf12f7114b8622
Gabriel Coelho <64917906+Gabocoelho8@users.noreply.github.com>
2023-11-05 18:46:53 -0400

    Fixed: general alignment and map
    fixed the alignment of all the information page and added the map with a photo of the building included

--- src/app/lab/LabTeachersInformation.jsx
+++ src/app/lab/LabTeachersInformation.jsx
@@ -6,7 +6,7 @@ import TeachersInformation from "@/components/teachersCard/teachersInformation";
 const LabTeachersInformation = () => {
     return (
         <>
-            <p className="pl-10 mt-10 mb-4 text-stratos px-4 text-3xl sm:text-3xl self-left tracking-tighter font-b612 font-bold" id="LabTeachersInformation">
+            <p className="pl-10 pb-3 mt-10 flex justify-center mb-4 text-stratos px-4 text-3xl sm:text-3xl self-left tracking-tighter font-b612 font-bold" id="LabTeachersInformation">
                 Nuestros Profesores
             </p>
             <div className= "lg:grid lg:grid-cols-2 lg:gap-4">
