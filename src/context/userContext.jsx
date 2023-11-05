3912bdde26179e9a1ddd4e0ef0e687fdfa82ed65
Erika Hernandez <erika.hernandez@correo.unimet.edu.ve>
2023-11-05 16:51:06 -0400

    feat: Login only with UnimetEmail

---
+++ src/context/userContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useEffect } from "react";
+import { onAuthStateChangedListener } from "../../firebase.js";
+
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => {},
+});
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+    {children}
+  </UserContext.Provider>;
+};
