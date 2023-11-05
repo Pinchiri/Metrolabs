037c6715f173cad367f07c9aa8b159bc4159fb99
Gabriel Coelho <64917906+Gabocoelho8@users.noreply.github.com>
2023-11-05 18:47:06 -0400

    Fixed: general alignment and map
    fixed the alignment of all the information page and added the map with a photo of the building included

--- src/app/lab/LabInformation.jsx
+++ src/app/lab/LabInformation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Laboratorio from "../../assets/Laboratorio.PNG";
+import Corimon from "../../assets/Captura5.PNG";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import MapSearchIcon from "mdi-react/MapSearchIcon";
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
@@ -7,10 +8,10 @@ import WatchLaterIcon from '@mui/icons-material/WatchLater';
 const LabInformation = () => {
   return (
     <>
-    <p className=" pl-10 mt-10 mb-4 text-stratos px-4 text-3xl sm:text-3xl self-left tracking-tighter font-b612 font-bold" id="LabInformation" >
+    <p className=" pl-10 mt-10 mb-4 text-stratos px-4 text-3xl flex justify-center sm:text-3xl self-left tracking-tighter font-b612 font-bold" id="LabInformation" >
         Ubicación del Laboratorio
     </p>
-    <div className="w-full flex flex-col mt-2 mb-10 " >
+    <div className="mt-2 mb-10 " >
       <img
         src={Laboratorio.src}
         alt="Laboratorio"
@@ -18,7 +19,7 @@ const LabInformation = () => {
       />
       
       <div className="lg:flex lg:flex-row lg:justify-around ">
-        <div className="pl-6 pt-5 w-full flex justify-left align-center  gap-2  ">
+        <div className="pl-6 pt-10 w-full flex justify-left pl-8 align-center  gap-2  ">
           <div className="flex justify-center items-center bg-[#FFB635] rounded-full h-14 w-14 animate-fade-left mx-4">
             <LocationOnIcon style={{ height: "35px", width: "35px" }} />
           </div>
@@ -27,7 +28,9 @@ const LabInformation = () => {
           </p>
         </div>
 
-        <div className="pl-6 pt-3 w-full flex items-left gap-2 lg:mt-4">
+        </div>
+
+        <div className="pt-3 w-full flex justify-left pl-8 items-left gap-2 lg:mt-4">
           <div className="flex justify-center items-center bg-[#FFB635] rounded-full h-14 w-14 animate-fade-left mx-4 ">
             <WatchLaterIcon style={{ height: "35px", width: "35px" }} />
           </div>
@@ -39,24 +42,26 @@ const LabInformation = () => {
               7:00am - 5:30pm  (previa reserva)
             </p>
           </div>
-        </div>
-        
-        
-        <div className="w-full flex gap-2 sm:justify-left pl-6">
-            <div className="w-1/2 ml-5 mt-5 text-oxford text-sm sm:text-2xl lg:text-3xl lg:self-center lg:justify-self-center sm:w-[150px] animate-fade-right">
+        </div>  
+      </div>
+        <div className="w-[auto] sm:justify-left flex justify-center rounded-lg">
+            <div className="w-[300px] h-[175px] lg:w-[600px] lg:h-[350px] sm:w-[400px] sm:h-[200px] ml-5 mt-5 text-oxford flex justify-center text-sm sm:text-2xl lg:text-3xl animate-fade-right">
                 <iframe 
-                    className="w-[250px] h-[150px] lg:w-[300px] lg:h-[200px]"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245.18862038205265!2d-66.78479482303842!3d10.499252747137666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a576d4b871293%3A0xa9c88b9110c38b31!2sF6X8%2BM6R%2C%20caminos%20internos%20Universidad%20Metropolitana%2C%20Caracas%201073%2C%20Miranda!5e0!3m2!1ses!2sve!4v1698094278576!5m2!1ses!2sve"
+                    className="w-[300px] h-[175px] lg:w-[600px] lg:h-[350px] sm:w-[400px] sm:h-[200px]"
+                    src="https://lenguasycontemporaneidad2.files.wordpress.com/2012/12/plano-acceso-unimet2.png"
                     allowfullscreen="" 
-                    loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade">
+                    loading="lazy">
                 </iframe>
             </div>
         </div>
-
-
-      </div> 
-    </div>
+        <div className="w-[auto] sm:justify-left flex justify-center rounded-lg">
+            <div className="w-[300px] h-[175px] lg:w-[600px] lg:h-[350px] sm:w-[400px] sm:h-[200px] ml-5 mt-5 text-oxford text-sm sm:text-2xl lg:text-3xl animate-fade-right">
+                <img
+                  src={Corimon.src}
+                  alt="Corimon"
+                />
+            </div>
+        </div>
   </>
   );
 };
