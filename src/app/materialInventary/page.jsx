15208a044a63af6468daecf10638adf15b0c3378
Erika Hernandez <erika.hernandez@correo.unimet.edu.ve>
2023-11-04 23:30:13 -0400

    feat: EditMaterialInventary ready w' skeleton

--- src/app/materialInventary/page.jsx
+++ src/app/materialInventary/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 
-
 import React, { useState, useEffect } from 'react';
 import MaterialCard from '@/components/materialCard/materialCard';
 import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear'; 
+import Spinner from '@/components/Spinner/spinner';
+
 
 
 const SheetComponent = () => {
@@ -12,29 +13,61 @@ const SheetComponent = () => {
   const [error, setError] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+  const [editData, setEditData] = useState(null);
 
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch('/api/sheetsMaterial');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+    if (editIndex !== null && editData !== null) {
+      updateData(editIndex, editData);
+    }
+  }, [editIndex, editData]);
+
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('/api/sheetsMaterial');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    };
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
-  }, []); 
+  }, []);
 
-  if (isLoading) return <div>Cargando...</div>;
+
+const updateData = async (rowIndex, rowData) => {
+  rowIndex= rowIndex + 4;
+  try {
+    const response = await fetch('/api/sheetsMaterialUpdate', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ rowIndex, rowData }), 
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const result = await response.json();
+    fetchData();
+
+  } catch (error) {
+    console.error('Failed to update data', error);
+  }
+};
+
+
+  if (isLoading) return <> <h1 className="font-['B612'] ml-10 mt-20 font-bold pt-5 text-3xl"> Inventario de Materiales </h1> <Spinner /> </> ;
   if (error) return <div>Fallo al cargar los datos: {error.message}</div>;
 
   const filteredData = data.filter(item =>
@@ -81,12 +114,15 @@ const SheetComponent = () => {
         {filteredData.map((item, index) => (
         <MaterialCard 
             key={index} 
+            index={index}
             material= {item.material} 
             capacity = {item.capacity}
             brand = {item.brand}
             quantity = {item.brand}
             ubication = {item.ubication}
             observations = {item.observations}
+            setEditIndex={setEditIndex}
+            setEditData={setEditData}
         />
 
       ))}
