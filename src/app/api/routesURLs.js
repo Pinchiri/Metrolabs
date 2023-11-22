f9cca8677d130ff52e8461a3a2ac29344751383c
Rolando Sorrentino <rolando.sorrentino@correo.unimet.edu.ve>
2023-11-22 18:26:38 -0400

    fix: :green_heart: Try to fix the production cache bug

---
+++ src/app/api/routesURLs.js
@@ -0,0 +1,1 @@
+export const sheetsReservationsURL = "/api/sheetsReservations";
--- src/app/api/sheetsReservations/route.js
+++ src/app/api/sheetsReservations/route.js
@@ -1,6 +1,9 @@
 import { google } from "googleapis";
 import { NextResponse } from "next/server";
 import { credentials } from "../googleConfig";
+import { revalidatePath } from "next/cache";
+import { sheetsReservationsURL } from "../routesURLs";
+import { NextRequest } from "next/server";
 
 const auth = new google.auth.GoogleAuth({
   credentials: credentials,
@@ -49,10 +52,25 @@ async function getSheetData() {
   }
 }
 
-export async function GET(req) {
+export async function GET(request) {
   try {
     const data = await getSheetData();
-    return NextResponse.json(data, { status: 200 });
+    const path = request.nextUrl.searchParams.get("path");
+    if (path) {
+      revalidatePath(path);
+      return NextResponse.json(data, {
+        revalidated: true,
+        now: Date.now(),
+        status: 200,
+      });
+    }
+
+    return NextResponse.json(data, {
+      revalidated: false,
+      now: Date.now(),
+      message: "Missing path to revalidate",
+      status: 200,
+    });
   } catch (error) {
     console.error(error); // Log the error to the console
     return NextResponse.json({ error: error.message }, { status: 500 });
